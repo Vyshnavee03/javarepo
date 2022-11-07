d15e2d45ba08a3c305a8aab8062c301850345f20
Vyshnavee03 <1000062034@hexaware.com>
2022-11-07 12:36:34 +0000

    push the code

---
+++ javacomp/src/main/java/com/javacomp/exception/EntityNotFoundException.java
@@ -0,0 +1,9 @@
+package com.javacomp.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+
+	public EntityNotFoundException(String exception) {
+		super(exception);
+	}
+
+}
